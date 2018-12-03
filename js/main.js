2e45a255b16528be975000fb30a6ba21f4e4c16b
kirillpotekhin <kirill.potekhin@gmail.com>
2018-12-03 17:25:12 +0700

    Сделал так чтобы на главном экране изображения не повторялись, поменял название массивов

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var randomInteger = function (min, max) {
+var getRandomNumber = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   rand = Math.floor(rand);
   return rand;
 };
 
-var mockComments = [
+var сommentSet = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -15,7 +15,7 @@ var mockComments = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-var mockNames = [
+var names = [
   'Иван',
   'Артем',
   'Тамара',
@@ -26,32 +26,31 @@ var mockNames = [
   'Елизаветта'
 ];
 
-var getPhotoDescription = function () {
-  var mockPhotoDescription = {};
-  var photoNumber = randomInteger(1, 25);
+var getPhotoDescription = function (photoNumber) {
+  var photoDescription = {};
 
-  mockPhotoDescription.url = 'photos/' + photoNumber + '.jpg';
+  photoDescription.url = 'photos/' + photoNumber + '.jpg';
 
-  mockPhotoDescription.likes = randomInteger(15, 200);
+  photoDescription.likes = getRandomNumber(15, 200);
 
-  var numberOfComments = randomInteger(1, mockComments.length);
+  var numberOfComments = getRandomNumber(1, сommentSet.length);
   var comments = [];
   for (var i = 0; i < numberOfComments; i++) {
     comments[i] = {};
-    comments[i].avatar = 'img/avatar-' + randomInteger(1, 6) + '.svg';
-    comments[i].message = mockComments[randomInteger(0, mockComments.length - 1)];
-    comments[i].name = mockNames[randomInteger(0, mockNames.length - 1)];
+    comments[i].avatar = 'img/avatar-' + getRandomNumber(1, 6) + '.svg';
+    comments[i].message = сommentSet[getRandomNumber(0, сommentSet.length - 1)];
+    comments[i].name = names[getRandomNumber(0, names.length - 1)];
   }
-  mockPhotoDescription.comments = comments;
+  photoDescription.comments = comments;
 
-  return mockPhotoDescription;
+  return photoDescription;
 };
 
 var CARDS_OF_NUMBERS = 25;
 
-var userPhotoList = [];
+var userPhotos = [];
 for (var j = 0; j < CARDS_OF_NUMBERS; j++) {
-  userPhotoList[j] = getPhotoDescription();
+  userPhotos[j] = getPhotoDescription(j + 1);
 }
 
 var picturesContainer = document.querySelector('.pictures');
@@ -68,7 +67,7 @@ var renderPictureDescription = function (picture) {
 
 var fragment = document.createDocumentFragment();
 for (var b = 0; b < CARDS_OF_NUMBERS; b++) {
-  fragment.appendChild(renderPictureDescription(userPhotoList[b]));
+  fragment.appendChild(renderPictureDescription(userPhotos[b]));
 }
 
 picturesContainer.appendChild(fragment);
@@ -76,7 +75,7 @@ picturesContainer.appendChild(fragment);
 var bigPicture = document.querySelector('.big-picture');
 bigPicture.classList.remove('hidden');
 
-var mainPhotoCard = userPhotoList[0];
+var mainPhotoCard = userPhotos[0];
 
 document.querySelector('.big-picture__img').children[0].src = mainPhotoCard.url;
 document.querySelector('.likes-count').textContent = mainPhotoCard.likes;
