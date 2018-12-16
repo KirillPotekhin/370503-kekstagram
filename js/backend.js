2c802fa1017a1a70267e08e87e0a66bffc1c0032
kirillpotekhin <kirill.potekhin@gmail.com>
2018-12-16 23:25:17 +0700

    Исправил объявление функции отправки и загрузки данных на сервер

--- js/backend.js
+++ js/backend.js
@@ -1,9 +1,7 @@
 'use strict';
 
 (function () {
-  var URL = 'https://js.dump.academy/kekstagram';
-
-  window.upload = function (data, onLoad, onError) {
+  var loadProcces = function (method, url, data, onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -23,35 +21,16 @@
 
     xhr.timeout = 10000;
 
-    xhr.open('POST', URL);
+    xhr.open(method, url);
+
     xhr.send(data);
   };
-})();
-
-(function () {
-  var URL = 'https://js.dump.academy/kekstagram/data';
 
   window.load = function (onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
+    loadProcces('GET', 'https://js.dump.academy/kekstagram/data', undefined, onLoad, onError);
+  };
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000;
-    xhr.open('GET', URL);
-
-    xhr.send();
+  window.upload = function (onLoad, onError) {
+    loadProcces('POST', 'https://js.dump.academy/kekstagram', 'data', onLoad, onError);
   };
 })();
